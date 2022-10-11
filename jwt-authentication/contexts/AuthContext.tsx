eb0ad67a004c8ece47df5f10b6b521bcfac4c848
Luis Ruediger <luisruediger27@gmail.com>
2022-10-11 14:19:00 -0300

    salvando tokens nos cookies

--- jwt-authentication/contexts/AuthContext.tsx
+++ jwt-authentication/contexts/AuthContext.tsx
@@ -39,8 +39,14 @@ export function AuthProvider({ children }: AuthProviderProps) {
   
       const { token, refreshToken, permissions, roles } = response.data;
 
-      setCookie(undefined, 'nextauth.token', token)
-      setCookie(undefined, 'nextauth.refreshToken', refreshToken)
+      setCookie(undefined, 'nextauth.token', token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/'
+      })
+      setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/'
+      })
 
       setUser({
         email,
