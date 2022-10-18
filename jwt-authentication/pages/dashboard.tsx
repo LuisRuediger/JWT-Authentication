07a9864e3e321d8bd8d01720a4e175329daee721
Luis Ruediger <luisruediger27@gmail.com>
2022-10-18 14:32:44 -0300

    validando permissoes

--- jwt-authentication/pages/dashboard.tsx
+++ jwt-authentication/pages/dashboard.tsx
@@ -14,7 +14,7 @@ export default function Dashboard() {
     api.get('/me')
       .then(response => console.log(response))
       .catch(err => console.log(err))
-  })
+  }, [])
   
   return ( 
     <>
